f6d473119b6089a81eeebbd6f1305782ccebcc94
Fernando Julian <nandokun69@gmail.com>
2023-06-04 12:51:46 +0700

    add carousel (not work yet)

---
+++ src/Component/Search.jsx
@@ -0,0 +1,13 @@
+import slider1 from "../img/slider1.jpeg";
+import slider2 from "../img/slider2.jpeg";
+import slider3 from "../img/slider3.jpeg";
+
+const Carousel = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Carousel;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
+import Carousel from "../Component/Carousel";
 import Navigation from "../Component/Navigation";
 
 const Home = () => {
   return (
     <>
       <Navigation />
+      <Carousel />
     </>
   );
 };
