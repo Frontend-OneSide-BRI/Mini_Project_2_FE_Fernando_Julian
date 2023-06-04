15d07888c5445a175a7a49a0b1b0216d1ac58dad
Fernando Julian <nandokun69@gmail.com>
2023-06-04 13:05:18 +0700

    add search bar on homepage

--- src/Component/Search.jsx
+++ src/Component/Search.jsx
@@ -1,13 +1,44 @@
-import slider1 from "../img/slider1.jpeg";
-import slider2 from "../img/slider2.jpeg";
-import slider3 from "../img/slider3.jpeg";
-
-const Carousel = () => {
+const Search = () => {
   return (
     <>
-      <div></div>
+      <div>
+        <div class="parallaxHeader">
+          <div class="titleParallaxHeader">
+            <h1 className="text-center text-3xl font-semibold">Search Here</h1>
+          </div>
+          <div class="searchParallaxHeader">
+            <div class="max-w-md mx-auto">
+              <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+                <div class="grid place-items-center h-full w-12 text-gray-300">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                  </svg>
+                </div>
+
+                <input
+                  class="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+                  type="text"
+                  id="search"
+                  placeholder="Search something.."
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
 
-export default Carousel;
+export default Search;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import Carousel from "../Component/Carousel";
 import Navigation from "../Component/Navigation";
+import Search from "../Component/Search";
 
 const Home = () => {
   return (
     <>
       <Navigation />
       <Carousel />
+      <Search />
     </>
   );
 };
